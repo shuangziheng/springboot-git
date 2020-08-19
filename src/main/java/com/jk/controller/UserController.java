e1a78b9d69c7cd416600c7560b8f385674f651bd
shuangziheng <1928351783@qq.com>
2020-08-19 14:48:14 +0800

    添加hello方法

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.jk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * description: UserController <br>
  * date: 2020/8/19 14:27 <br>
@@ -7,4 +9,9 @@ package com.jk.controller;
  * version: 1.0 <br>
  */
 public class UserController {
+
+    @RequestMapping("/hello")
+    private void hello(){
+
+    }
 }
