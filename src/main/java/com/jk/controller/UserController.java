e62d8f133ed6d14174e879b9de12f5cd199c507f
shuangziheng <1928351783@qq.com>
2020-08-19 15:11:19 +0800

    添加controller注解

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.jk.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * description: UserController <br>
  * date: 2020/8/19 14:27 <br>
  * author: shuang <br>
  * version: 1.0 <br>
  */
+@Controller
 public class UserController {
 }
