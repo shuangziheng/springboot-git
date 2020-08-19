bddee63853fdd29c01eceb7da8ced822cf0da328
shuangziheng <1928351783@qq.com>
2020-08-19 14:31:12 +0800

    注添加用户controller类

---
+++ src/main/java/com/jk/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.jk.controller;
+
+/**
+ * description: UserController <br>
+ * date: 2020/8/19 14:27 <br>
+ * author: shuang <br>
+ * version: 1.0 <br>
+ */
+public class UserController {
+}
